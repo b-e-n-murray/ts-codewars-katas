12044cbec4414b5b84254a940656730c292f2435
Ben Murray <benmurray573@gmail.com>
2022-11-06 14:40:16 +0000

    added test cases for ordered-substrings

---
+++ src/ordered-substring.test.ts
@@ -0,0 +1,11 @@
+import inArray from "./ordered-substring";
+
+test("conatins two, no rearrangement", () => {
+  expect(inArray(["xyz", "live", "strong"], ["lively", "alive", "harp", "sharp", "armstrong"]).toBe(["live", "strong"]);
+});
+test("contains three, with rearrangement", () => {
+    expect(inArray(["live", "strong", "arp"], ["lively", "alive", "harp", "sharp", "armstrong"]).toBe(["arp", "live", "strong"]);
+  });
+  test("contains no substrings", () => {
+  expect(inArray(["tarp", "mice", "bull"], ["lively", "alive", "harp", "sharp", "armstrong"]).toBe([]);
+})
